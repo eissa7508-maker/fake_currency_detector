224195e89634917f91e60b8021c2ffc9c42d3c55
Eiss <eissa7508@gmail.com>
2026-05-07 02:45:04 +0200

    fix project clean requirements

---
+++ main.py
@@ -0,0 +1,37 @@
+from fastapi import FastAPI, File, UploadFile
+import numpy as np
+import tensorflow as tf
+from PIL import Image
+import io
+
+app = FastAPI()
+
+# تحميل الموديل
+interpreter = tf.lite.Interpreter(model_path="currency_model.tflite")
+interpreter.allocate_tensors()
+
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
+
+@app.get("/")
+def home():
+    return {"message": "Fake Currency API is working 🚀"}
+
+@app.post("/predict")
+async def predict(file: UploadFile = File(...)):
+    image = await file.read()
+
+    img = Image.open(io.BytesIO(image)).convert("RGB")
+    img = img.resize((224, 224))
+
+    img = np.array(img, dtype=np.float32) / 255.0
+    img = np.expand_dims(img, axis=0)
+
+    interpreter.set_tensor(input_details[0]['index'], img)
+    interpreter.invoke()
+
+    output = interpreter.get_tensor(output_details[0]['index'])
+
+    result = "Fake 💰" if output[0][0] > 0.5 else "Real 💵"
+
+    return {"result": result}
